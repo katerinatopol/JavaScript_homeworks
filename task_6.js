c2fafabfa54e394a541bc3cecfb1eba31ce0deeb
katerina_topol <katerina_topol@mail.ru>
2021-05-07 17:58:35 +0300

    upd

--- task_6.js
+++ task_6.js
@@ -3,3 +3,21 @@
 В зависимости от переданного значения операции выполнить одну из арифметических операций 
 (использовать функции из пункта 5) и вернуть полученное значение (использовать switch). */
 
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case '+':
+            return arg1 + arg2;
+        case '-':
+            return arg1 - arg2;
+        case '*':
+            return arg1 * arg2;
+        case '/':
+            return parseInt(arg1 / arg2);
+        default:
+            return 'Вы указали что-то не то';
+    }
+}
+
+
+let userPrompt = prompt('Напишите выражение, которое нужно посчитать (пример: 4 + 2): ').split(' ');
+alert(mathOperation(+userPrompt[0], +userPrompt[2], userPrompt[1]));
