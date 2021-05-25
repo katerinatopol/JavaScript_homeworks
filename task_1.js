fd8fd5ba81b94c2d1c768678c7c822ff2437eba9
katerina_topol <katerina_topol@mail.ru>
2021-05-25 17:12:47 +0300

    add task_2

--- task_1.js
+++ task_1.js
@@ -40,3 +40,66 @@ function chessBoard() {
 
 
 document.body.appendChild(chessBoard());
+
+
+/*
+2. Сделать генерацию корзины динамической: верстка корзины не должна находиться в HTML-структуре. 
+Там должен быть только div, в который будет вставляться корзина, сгенерированная на базе JS:
+2.1. Пустая корзина должна выводить строку «Корзина пуста»;
+2.2. Наполненная должна выводить «В корзине: n товаров на сумму m рублей».
+*/
+
+function Product(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+
+    this.addInBasket = function (basket) {
+        basket.contentBasket.push(this)
+    };
+}
+
+
+let basket = {
+    contentBasket: [],
+
+    countBasketPrice() {
+        return this.contentBasket.reduce((totalPrice, cartItem) =>
+            totalPrice + cartItem.price * cartItem.count, 0);
+    }
+
+};
+
+const pear = new Product('pear', 80, 1);
+const apple = new Product('apple', 100, 2);
+const orange = new Product('orange', 120, 3);
+
+
+pear.addInBasket(basket);
+apple.addInBasket(basket);
+orange.addInBasket(basket);
+
+
+function render(basketUser) {
+    let div = document.querySelector('#cart');
+    let resCount = 0;
+
+    let allPrice = basketUser.countBasketPrice();
+    basketUser.contentBasket.forEach(el => {
+        resCount += el.count;
+    });
+    //console.log(resCount);
+    //console.log(basketUser.countBasketPrice());
+
+    if (allPrice === 0 && resCount === 0) {
+        div.insertAdjacentText('afterbegin', 'Корзина пуста');
+    }
+    else {
+        div.insertAdjacentText('afterbegin', `В корзине: ${resCount}
+        товаров на сумму ${allPrice} рублей`);
+    }
+    
+};
+    
+
+render(basket);
